2f8492957bceede4495208a3cdef1a57097bfc53
Dongjun Yu <djyu1210@gmail.com>
2015-06-11 06:47:46 -0500

    updated.. under construction

--- R/hotelhd_control.R
+++ R/hotelhd_control.R
@@ -3,36 +3,47 @@
 #' Various parameters that control setting of tests. Currently,
 #' the maximum type tests (i.e., 'CLX', 'Z', 'M') only need control parameters.
 #'
-#' @param C a constant to decide lambda_n for 'CLX', See Cai et al. (2011).
-#' @param omegaHat character, "omega" or"identity".
-#'   A candidate estimate of Omega matrix for the two
-#'   maximum
-#'   type metods, 'CLX' and 'Z'. When the default 'omega' is used,
-#'   Omega will be estimated, See 'omegaHat' argument. Optionally,
-#'   'identity' can be choosen to set p by p Identity matrix as
-#'   the estimate of Omega matrix.
-#' @param omegaEst character (c("clime")).
-#'   The estimation method for \eqn{\Omega} matrix,
-#'   which is used for maximum type tests.
-#'   Currently, only 'clime' is implemented; see \code{flare::sugm}.
-#' @param omegaGiven If an omega was given, use it. This is an optional but
-#'   is included to improve speed of simulation study.
-#'   All the methods using 'clime' package will use the given omega,
-#'   so do not need to calculate individually.
-#' @param subForM for the method 'M'. 'sub' extract sub-matrix from Omega.
-#'   'diag' use diagonals of Omega (to improve efficiency)
-#' @param ndim The number of dimensions for generalized maximum type statistic.
-#'   Default value is 4.
-#' @param R The number of bootstrap statistics for 'Z'.
-#' @param block A block size for blockwize multiplier
+#' @param C numeric. to decide lambda_n for 'CLX', See Cai et al. (2011).
+#'
+#' @param omegaGiven \code{NULL} or \eqn{p} by \eqn{p} matrix.
+#'   When the default \code{NULL} is chosen, \eqn{\Omega} matrix is estimated;
+#'   see \code{omegaHat} and \code{omegaEst} parameters.
+#'   Optionally, users can provide an estimate of \eqn{\Omega}
+#'   from an estimation method that this package does not offer.
+#'
+#' @param omegaHat character; 'omega' or 'identity'.
+#'   A candidate estimate of \eqn{\Omega} matrix for the maximum type methods.
+#'   When the default of 'omega' is used, \eqn{\Omega} is estimated.
+#'   Users can choose estimation method by \code{omegaEst} parameter.
+#'   Optionally,
+#'   'identity' can be choosen to set \eqn{p} by \eqn{p} Identity matrix as
+#'   an estimate of \eqn{\Omega} matrix.
+#'
+#' @param omegaEst character; 'clime'.
+#'   The estimation method for \eqn{\Omega} matrix.
+#'   Currently, only 'clime' is implemented; see \code{\link[flare]{sugm}}.
+#'
+#' @param ndim integer. The number of dimensions for generalized maximum
+#'   type statistic, i.e., the method 'M'. The default is 4.
+#'
+#' @param sub character; 'diag' or 'sub'.
+#'   Method about how to form \code{ndim} by \code{ndim} sub-matrix for
+#'   the method 'M'. The default 'diag' uses diagonals of \eqn{\Omega}
+#'   to improve efficiency, and calculate results of 1 to \code{ndim}.
+#'   The 'sub' extracts sub-matrix from \eqn{\Omega}.
+
+#' @param B integer. The number of bootstrap statistics for 'Z'.
+#'
+#' @param block integer. A block size for blockwize multiplier
 #'   bootstrap in the method 'Z'. The size should be smaller
 #'   than min(n1, n2). The default value is 1.
 #'
 #' @export
-hotelhd_control <- function(C = 10, omegaHat = c("omega", "identity"),
-                            omegaEst = c("clime"), omegaGiven = NULL,
-                            subForM = c("sub", "diag"), ndim=4,
-                            R = 500, block = 1)
+hotelhd_control <- function(C=10, omegaGiven=NULL,
+                            omegaHat=c("omega", "identity"),
+                            omegaEst=c("clime"),
+                            subForM=c("diag", "sub"), ndim=4L,
+                            B=500L, block=1L)
 {
   #method <- match.arg(method)
 
